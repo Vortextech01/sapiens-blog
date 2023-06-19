09cfcd502987c05f145b699d29aa688447cf406d
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-06-19 03:11:29 -0600

    Update themes.js

--- themes.js
+++ themes.js
@@ -1,53 +1,53 @@
 exports.COLOR_THEMES = {
   default: {
     colors: {
-      primary: '#7D7AFF',
-      'gradient-1': '#7d7aff',
-      'gradient-2': '#2121e2',
-      'gradient-3': '#00fff0',
-      'gradient-4': '#8785FF',
+      primary: '#00FFB8',
+      'gradient-1': '#1a1a1a',
+      'gradient-2': '#00FFB8',
+      'gradient-3': '#1a1a1a',
+      'gradient-4': '#00FFB8',
     },
   },
   bejamas: {
     colors: {
       primary: '#FF8585',
-      'gradient-1': '#7d7aff',
-      'gradient-2': '#2121E2',
-      'gradient-3': '#FF76B8',
-      'gradient-4': '#001AFF',
+      'gradient-1': '#1a1a1a',
+      'gradient-2': '#FF8585',
+      'gradient-3': '#1a1a1a',
+      'gradient-4': '#FF8585',
     },
   },
   netlify: {
     colors: {
-      primary: '#00A354',
-      'gradient-1': '#00F0FF',
-      'gradient-2': '#00F0FF',
-      'gradient-3': '#FAFF00',
-      'gradient-4': '#00F0FF',
+      primary: '#00FFB8',
+      'gradient-1': '#1a1a1a',
+      'gradient-2': '#00FFB8',
+      'gradient-3': '#1a1a1a',
+      'gradient-4': '#00FFB8',
     },
   },
   reddie: {
     colors: {
       primary: '#FF4D4D',
-      'gradient-1': '#FFC700',
-      'gradient-2': '#FF85DD',
-      'gradient-3': '#FF85DD',
-      'gradient-4': '#FF8585',
+      'gradient-1': '#1a1a1a',
+      'gradient-2': '#FF4D4D',
+      'gradient-3': '#1a1a1a',
+      'gradient-4': '#FF4D4D',
     },
   },
   greenie: {
     colors: {
       primary: '#C78500',
-      'gradient-1': '#FFCC81',
-      'gradient-2': '#00F37F',
-      'gradient-3': '#00F37F',
-      'gradient-4': '#FFCC81',
+      'gradient-1': '#1a1a1a',
+      'gradient-2': '#C78500',
+      'gradient-3': '#1a1a1a',
+      'gradient-4': '#C78500',
     },
   },
 };
 
 exports.FONT_THEMES = {
-  'sans-serif': `ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
-  serif: `ui-serif, Georgia, Cambria, "Times New Roman", Times, serif`,
-  monospace: `ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace`,
+  'sans-serif': 'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+  serif: 'ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+  monospace: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
 };
